832ff175a61062a221d7152279e58e1340f29e83
Dinesh <111186843+dineshahire@users.noreply.github.com>
2024-04-19 10:42:33 +0530

    Add files via upload

---
+++ exp10.py
@@ -0,0 +1,22 @@
+import numpy arr = numpy.array ([1, 2, 3, 4, 5]) print(arr)
+
+#OUTPUT: [1,2,3,4,5]
+
+#CODE:
+import numpy as np print (np. version )
+
+#OUTPUT:
+1.16.3
+
+#CODE:
+import numpy as
+np arr = np.array([1, 2, 3, 4])
+print(arr) 
+print(type(arr))
+
+#OUTPUT:
+[1 2 3 4 5]
+<class 'numpy.ndar ray'>
+
+#CODE:
+import numpy as np arr = np.array(42) print(arr)
