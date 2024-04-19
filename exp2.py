6126110c7522da0fdca407aeaaa7edae60b8ff0c
Dinesh <111186843+dineshahire@users.noreply.github.com>
2024-04-19 10:32:35 +0530

    Add files via upload

---
+++ exp2.py
@@ -0,0 +1,16 @@
+#conditional statment 
+a=101 
+b=255
+if a > b:
+print("a is greater than b") 
+elif a== b:
+print("a and b are equal")
+else:
+print("b is greater than a")
+
+#for loop
+languages =["C", "C+","C++"] 
+for x in languages: print(x)
+
+
+
